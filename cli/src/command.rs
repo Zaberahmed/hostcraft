204c93205eedd97b5dc9cd0b50bfa8d6038d4ad9
Sk. Zaber Ahmed <zaber0305@gmail.com>
2026-03-25 22:39:34 +0600

    modify: updated error messages

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -61,7 +61,7 @@ pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
 
         Command::Add { name, ip } => {
             host::add_entry(&mut entries, ip, name.clone()).map_err(|e| match e {
-                HostError::DuplicateEntry => format!("Entry already exists: {}", e),
+                HostError::DuplicateEntry => format!("Entry already exists. {}", e),
                 _ => format!("Failed to add entry: {}", e),
             })?;
 
@@ -74,7 +74,7 @@ pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
 
         Command::Remove { name } => {
             host::remove_entry(&mut entries, &name).map_err(|e| match e {
-                HostError::EntryNotFound => format!("No entry found matching '{}': {}", name, e),
+                HostError::EntryNotFound => format!("No entry found matching '{}'. {}", name, e),
                 _ => format!("Failed to remove entry: {}", e),
             })?;
 
@@ -87,7 +87,7 @@ pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
 
         Command::Toggle { name } => {
             host::toggle_entry(&mut entries, &name).map_err(|e| match e {
-                HostError::EntryNotFound => format!("No entry found matching '{}': {}", name, e),
+                HostError::EntryNotFound => format!("No entry found matching '{}'. {}", name, e),
                 _ => format!("Failed to toggle entry: {}", e),
             })?;
 
