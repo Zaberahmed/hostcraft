9e3138f7c7cc7d1f7dd1306e69ba0bbc4f7ae574
Sk. Zaber Ahmed <zaber0305@gmail.com>
2026-04-05 07:52:52 +0600

    update: file related func signatures

--- core/src/file/mod.rs
+++ core/src/file/mod.rs
@@ -7,19 +7,19 @@ use std::{
     path::Path,
 };
 
-pub fn read_file<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+pub fn read_file<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
-    let file = File::open(filename)?;
+    let file = File::open(file_path)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-pub fn write_file<P>(filename: P, entries: &[HostEntry]) -> io::Result<()>
+pub fn write_file<P>(file_path: P, entries: &[HostEntry]) -> io::Result<()>
 where
     P: AsRef<Path>,
 {
-    let mut file = File::create(filename)?;
+    let mut file = File::create(file_path)?;
     write_entries(entries, &mut file)?;
     Ok(())
 }
