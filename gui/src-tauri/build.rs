aae6ad71d78680a18e780cccf4f590ff1609da84
Sk. Zaber Ahmed <zaber0305@gmail.com>
2026-04-08 15:51:08 +0600

    update: simplified build.rs to avoid lint warning

--- gui/src-tauri/build.rs
+++ gui/src-tauri/build.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let mut attributes = tauri_build::Attributes::new();
+    #[cfg(not(target_os = "windows"))]
+    let attributes = tauri_build::Attributes::new();
 
     #[cfg(target_os = "windows")]
-    {
-        attributes = attributes.windows_attributes(
-            tauri_build::WindowsAttributes::new().app_manifest(include_str!("app.manifest")),
-        );
-    }
+    let attributes = tauri_build::Attributes::new().windows_attributes(
+        tauri_build::WindowsAttributes::new().app_manifest(include_str!("app.manifest")),
+    );
 
     tauri_build::try_build(attributes).expect("failed to run tauri-build");
 }
